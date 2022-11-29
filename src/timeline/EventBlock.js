4ddb2e27d3d2ed543076b8f4b5ae9dd472a83a46
Apple Stackup <applestackup@Apples-MacBook-Pro.local>
2022-11-29 21:24:48 +0500

    event block ui fixes

--- src/timeline/EventBlock.js
+++ src/timeline/EventBlock.js
@@ -21,7 +21,15 @@ const EventBlock = (props) => {
     const _onPress = useCallback(() => {
         onPress(index);
     }, [index, onPress]);
-    return (<TouchableOpacity activeOpacity={0.9} onPress={_onPress} style={[styles.event, eventStyle]}>
+    return (<TouchableOpacity activeOpacity={0.9} onPress={_onPress} style={[styles.event, eventStyle, {
+        marginLeft: 12, 
+        height: event?.height -20,
+        marginTop: 12,
+        // alignItems: "center",
+        justifyContent: "center"
+    }]}>
+
+    
         {renderEvent ? (renderEvent(event)) : (<View>
           {/* <Text numberOfLines={1} style={styles.eventTitle}>
             {event.title || 'Event'}
@@ -32,8 +40,11 @@ const EventBlock = (props) => {
           {/* {numberOfLines > 2 ? (<Text style={styles.eventTimes} numberOfLines={1}>
               {new XDate(event.start).toString(formatTime)} - {new XDate(event.end).toString(formatTime)}
             </Text>) : null} */}
+
+
+            
             {numberOfLines > 2 ? (
-                <View style={{ flexDirection: "row", justifyContent: "space-between", width: "95%", marginTop: 10 }}>
+                <View style={{ flexDirection: "row", justifyContent: "space-between", width: "95%" }}>
                     <Text style={{ fontWeight: "bold" }}>
                         {event.studentName}
                     </Text>
@@ -47,8 +58,10 @@ const EventBlock = (props) => {
                     <View style={{ flexDirection: "row", justifyContent: "space-between", width: "95%", marginTop: 5 }}>
                     <View style={{ flexDirection: "row", alignItems: "center" }}>
                         <Image source={event.locationPin} style={{height: 16, width: 16, tintColor: "gray", marginTop: 5}} />
-                        <Text style={{...styles.eventTimes, fontSize: 14, marginLeft: 5}}>   
-                            {event.city}
+                            <Text style={{ ...styles.eventTimes, fontSize: 12, marginLeft: 5, width: "50%" }}
+                            numberOfLines={1}
+                            >   
+                            {event.pickupLocation}
                         </Text>
                     </View>
                     <View style={{ flexDirection: "row", alignItems: "center" }}>
